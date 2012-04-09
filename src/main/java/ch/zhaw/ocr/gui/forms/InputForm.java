e5f1a3e07f4f71248d5e741a550e2d52fe7280cb
Zeugin Corinne <zeugicor@students.zhaw.ch>
2012-04-09 19:01:02 +0200

    InputForm refactored

--- src/main/java/ch/zhaw/ocr/gui/forms/InputForm.java
+++ src/main/java/ch/zhaw/ocr/gui/forms/InputForm.java
@@ -3,14 +3,18 @@ package ch.zhaw.ocr.gui.forms;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileFilter;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
 public class InputForm {
@@ -18,9 +22,7 @@ public class InputForm {
 	final JFileChooser fc = new JFileChooser();
 	
     private JFrame frame;
-    private JPanel panel;
-    private JPanel areaPanel;
-    private JPanel buttonPanel;
+    private JTextField imagePath;
     private JTextArea analysedText;
     private JButton saveButton;
     private JButton browseButton;
@@ -40,22 +42,31 @@ public class InputForm {
     
     private void openWindow() {
     	
-        frame = new JFrame("Dumdidum");
+        frame = new JFrame("");
         frame.setLocation(400, 300);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setLayout(new BorderLayout());
         
         JPanel contentPane = new JPanel(new BorderLayout());
         
-        panel = new JPanel(new SpringLayout());
-        areaPanel = new JPanel(new BorderLayout());
-        buttonPanel = new JPanel(new SpringLayout());
+        frame.getContentPane().add(contentPane);
+       
+        contentPane.add(loadTextArea(), BorderLayout.CENTER);
+        contentPane.add(loadButtons(), BorderLayout.SOUTH);
+        
+        frame.setVisible(true);
+        frame.pack();
+
+    }
+    
+    private JComponent loadButtons() {
+    	
+        JPanel buttonPanel = new JPanel(new SpringLayout());
         
         JLabel browseImg = new JLabel("Browse Image:");
         
-        analysedText = new JTextArea(20,70);
-        
-        saveButton = new JButton("Save as text");
+        imagePath = new JTextField();
+
         browseButton = new JButton("Browse");
         analyseButton = new JButton("Analyse");
         
@@ -71,38 +82,44 @@ public class InputForm {
 					
 			        if (returnVal == JFileChooser.APPROVE_OPTION) {
 			            java.io.File file = fc.getSelectedFile();
+			            imagePath.setText(file.getPath());
 			        } 
 				}
 				
 			}
         });
         
-        panel.add(new JLabel());
-        
-        areaPanel.add(analysedText);
-        
-        buttonPanel.add(new JLabel());
-        buttonPanel.add(new JLabel());
-        buttonPanel.add(new JLabel());
-        buttonPanel.add(saveButton);
         buttonPanel.add(browseImg);
-        buttonPanel.add(new JLabel());
+        buttonPanel.add(imagePath);
         buttonPanel.add(browseButton);
         buttonPanel.add(analyseButton);
         
-        frame.getContentPane().add(contentPane);
+        SpringUtilities.makeCompactGrid(buttonPanel, 1, 4, 5, 5, 5, 5);
         
-        SpringUtilities.makeCompactGrid(panel, 1, 1, 5, 5, 5, 5);
-        SpringUtilities.makeCompactGrid(buttonPanel, 2, 4, 5, 5, 5, 5);
-
+        return buttonPanel;
+    	
+    }
+    
+    private JComponent loadTextArea() {
+    	
+        JPanel areaPanel = new JPanel(new SpringLayout());
+        JPanel button2Panel = new JPanel();
         
-        contentPane.add(panel, BorderLayout.NORTH);
-        contentPane.add(areaPanel, BorderLayout.CENTER);
-        contentPane.add(buttonPanel, BorderLayout.SOUTH);
+        analysedText = new JTextArea(20,70);
+        JScrollPane scrollAnalysed = new JScrollPane (analysedText);
         
-        frame.setVisible(true);
-        frame.pack();
-
+        saveButton = new JButton("Save as text");
+        
+        button2Panel.setLayout(new BoxLayout(button2Panel, BoxLayout.LINE_AXIS));
+        button2Panel.add(Box.createHorizontalGlue());
+        button2Panel.add(saveButton);
+        
+        areaPanel.add(scrollAnalysed);
+        areaPanel.add(button2Panel);
+                
+        SpringUtilities.makeCompactGrid(areaPanel, 2, 1, 10, 10, 10, 20);
+    	
+        return areaPanel;
     }
 
 }
