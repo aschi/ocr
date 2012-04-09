40c30a6ba8f7650e0260cd08c4acdb5a1279fbe5
Priscilla <schnepri@students.zhaw.ch>
2012-04-09 16:56:54 +0200

    Emphasis-Vektor für die Gewichtung unterschiedlicher Vergleichskriterien impementiert

--- src/main/java/ch/zhaw/ocr/BitmapParser/BitmapParser.java
+++ src/main/java/ch/zhaw/ocr/BitmapParser/BitmapParser.java
@@ -1,9 +1,7 @@
 package ch.zhaw.ocr.BitmapParser;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.util.List;
-import ch.zhaw.ocr.*;
 
 public interface BitmapParser {
 	public void parse(BufferedImage image);
--- src/main/java/ch/zhaw/ocr/CharacterRecognition/Character.java
+++ src/main/java/ch/zhaw/ocr/CharacterRecognition/Character.java
@@ -5,6 +5,7 @@ import ch.zhaw.ocr.BitmapParser.ContrastMatrix;
 public class Character implements Comparable<Character>{
 	private ContrastMatrix characterM;
 	private int[] comparisonVector = new int[29];
+	private int[] emphasisVector = new int[29];
 
 	/*
 	 * comparisonVector: Split character matrix into 25 parts (5x5) 
@@ -21,6 +22,7 @@ public class Character implements Comparable<Character>{
 	 * @param characterM
 	 */
 	public Character(ContrastMatrix characterM) {
+		setEmphasis();
 		characterM.trim();
 		this.characterM = characterM;
 
@@ -81,6 +83,30 @@ public class Character implements Comparable<Character>{
 		}
 
 	}
+	
+	//All emphasises have to bet set
+	//The emphasises are set for each field from comparison Vector
+	private void setEmphasis() {
+		/*
+		 * comparisonVector: Split character matrix into 25 parts (5x5) 
+		 * 0-24: Number of relevant pixels in each of the 10 parts. scaled to 10 
+		 * 25: Number of relevant pixels in top half. scaled to 10 
+		 * 26: Number of relevant pixels in bottom half. scaled to 10 
+		 * 27: width / height ratio scaled to 10
+		 * 28: indicator for vertically disjoint character (like i or j) (0 or 1)
+		 */
+		//0-24 should have the same emphasis
+		for (int i = 0; i <= 24; i++) {
+			emphasisVector[i] = 1;
+		}
+		//25/26: should have a larger emphasis than fields 0-24
+		emphasisVector[25] = 12;
+		emphasisVector[26] = emphasisVector[25];
+		//27: should have nearly the same emphasis like field 25/26
+		emphasisVector[27] = 15;
+		//28: should have the highest emphasis, because there can't be f.e. an "a" with a disjointed character
+		emphasisVector[28] = 50;
+	}
 
 	public ContrastMatrix getMatrix() {
 		return characterM;
@@ -104,11 +130,11 @@ public class Character implements Comparable<Character>{
 		
 		for(int i = 0;i < comparisonVector.length;i++){
 			if(comparisonVector[i] != ((Character)o).getComparisonVector()[i]){
-				diff+= Math.abs(comparisonVector[i]-((Character)o).getComparisonVector()[i]);
+				diff+= Math.abs(comparisonVector[i]-((Character)o).getComparisonVector()[i]) * emphasisVector[i];
 			}
 		}
 		
-		if(diff < 10){
+		if(diff < 50){
 			return true;
 		}else{
 			return false;
