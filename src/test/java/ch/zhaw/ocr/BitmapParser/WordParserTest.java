a24f272efa9619c0fadfcc06b31650c0775b76b8
Adrian Schmid <schmiad1@students.zhaw.ch>
2012-04-12 21:36:38 +0200

    WordParser Test angefangen

--- src/test/java/ch/zhaw/ocr/BitmapParser/WordParserTest.java
+++ src/test/java/ch/zhaw/ocr/BitmapParser/WordParserTest.java
@@ -2,15 +2,27 @@ package ch.zhaw.ocr.BitmapParser;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JUnit4Mockery;
 import org.junit.Before;
 import org.junit.Test;
 
 public class WordParserTest {
 	private ContrastMatrix exampleMatrix;
+	private List<ContrastMatrix> resultList;
+	
+	private Mockery context;
+	
 	
 	@Before
 	public void setUp() throws Exception {
-		exampleMatrix = new ContrastMatrix(20, 5);
+		context = new JUnit4Mockery();
+				
+		exampleMatrix = new ContrastMatrix(40, 5);
 		
 		fillCol(exampleMatrix, 0);
 		fillCol(exampleMatrix, 1);
@@ -23,9 +35,41 @@ public class WordParserTest {
 		fillCol(exampleMatrix, 9);
 		fillCol(exampleMatrix, 10);
 	
+		fillCol(exampleMatrix, 13);
+		fillCol(exampleMatrix, 14);
+		fillCol(exampleMatrix, 15);
+		fillCol(exampleMatrix, 16);
 		fillCol(exampleMatrix, 17);
-		fillCol(exampleMatrix, 18);
-		fillCol(exampleMatrix, 19);
+		
+		
+		fillCol(exampleMatrix, 27);
+		fillCol(exampleMatrix, 28);
+		fillCol(exampleMatrix, 29);
+		fillCol(exampleMatrix, 30);
+		
+		fillCol(exampleMatrix, 33);
+		fillCol(exampleMatrix, 34);
+		
+		fillCol(exampleMatrix, 35);
+		fillCol(exampleMatrix, 36);
+		fillCol(exampleMatrix, 37);
+		fillCol(exampleMatrix, 38);
+		fillCol(exampleMatrix, 39);
+		
+		
+		//set up desired result		
+		resultList= new LinkedList<ContrastMatrix>();
+		ContrastMatrix cm = new ContrastMatrix(4, 5);
+		cm.invertMatrix();
+		resultList.add(cm);
+		
+		cm = new ContrastMatrix(5, 5);
+		cm.invertMatrix();
+		resultList.add(cm);
+		
+		cm = new ContrastMatrix(3, 5);
+		cm.invertMatrix();
+		resultList.add(cm);
 	}
 
 	private void fillCol(ContrastMatrix cm, int colNo){
@@ -36,7 +80,21 @@ public class WordParserTest {
 
 	@Test
 	public void testParse() {
-		fail("Not yet implemented");
-	}
+		//input matrices
+		final List<ContrastMatrix> matrices = new LinkedList<ContrastMatrix>();
+		matrices.add(exampleMatrix);
+		
+		
+		final BitmapParser bp = context.mock(BitmapParser.class);
+		
+		context.checking(new Expectations() {{
+			oneOf (bp).parse(null); will(returnValue(matrices));
+	    }});
 
+		
+		BitmapParser instance = new WordParser(bp);
+		List<ContrastMatrix> parsedList = instance.parse(null);
+		
+		assertTrue(parsedList.equals(resultList));
+	}
 }
