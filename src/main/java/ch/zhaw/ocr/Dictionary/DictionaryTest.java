790c9fc37125ab9b912149d239fa1b989d934310
Adrian Schmid <schmiad1@students.zhaw.ch>
2012-05-02 14:33:37 +0200

    Zeitmessung eingefügt...

--- src/main/java/ch/zhaw/ocr/Dictionary/DictionaryTest.java
+++ src/main/java/ch/zhaw/ocr/Dictionary/DictionaryTest.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 public class DictionaryTest {
 	public static void main(String[] args) {
 		try {
+			long t1 = System.currentTimeMillis();
 			Dictionary dict = new Dictionary(new File("res/german.dic"));
-			System.out.println(dict.correctWord("qute"));
+			System.out.println("dictionary loaded...(" + (System.currentTimeMillis()-t1) +"ms)");
+			long t2 = System.currentTimeMillis();
+			System.out.println(dict.correctWord("qute") + " ("+ (System.currentTimeMillis()-t2) +"ms)");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
