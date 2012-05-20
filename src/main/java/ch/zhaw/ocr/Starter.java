b2e405d44aa073b09189de21ea82e5d3d1be0ca7
Priscilla <schnepri@students.zhaw.ch>
2012-05-20 16:58:08 +0200

    Working Material

--- src/main/java/ch/zhaw/ocr/Starter.java
+++ src/main/java/ch/zhaw/ocr/Starter.java
@@ -13,29 +13,29 @@ public class Starter {
 	private static Dictionary dict = null;
 
 	public static void main(String[] args) {
-		Thread t = new Thread(new Runnable() {
-			public void run() {
+//		Thread t = new Thread(new Runnable() {
+//			public void run() {
 				try {
 					dict = new Dictionary("production");
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			}
-		});
+//			}
+//		});
 		
 
-		Thread t2 = new Thread(new Runnable() {
-			public void run() {
+//		Thread t2 = new Thread(new Runnable() {
+//			public void run() {
 				CharacterComperator cc = new CharacterComperator(dict);
 				InitialLearning.learn(cc);
 				Ocr ocr = new Ocr(cc, dict);
 
 				new MainGui(ocr);
-			}
-		});
-		t2.run();
-		t.run();
+//			}
+//		});
+//		t2.run();
+//		t.run();
 
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			public void run() {
